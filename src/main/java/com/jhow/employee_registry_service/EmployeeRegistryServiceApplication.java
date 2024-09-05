755c4f97bf69cdc633fd300b1cf3613cb5feb8ce
jhowlacerda <jonatha.lacerda@outlook.com>
2024-09-04 23:05:25 -0300

    Implementacao inicial

---
+++ src/main/java/com/jhow/employee_registry_service/EmployeeRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jhow.employee_registry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhow/employee_registry_service/EmployeeRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhow.employee_registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
